a0114623ed05f2946d1c7559719ee8cc5c704d2d
Gary Haran <gary.haran@gmail.com>
2022-10-17 20:17:25 -0400

    focus the specified gender field when selecting to write your own in

--- app/javascript/controllers/profile_form_controller.js
+++ app/javascript/controllers/profile_form_controller.js
@@ -2,14 +2,19 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="profile"
 export default class extends Controller {
-  static targets = ['gender', 'extras']
+  static targets = ['gender', 'extras', 'specified']
 
   connect() {
     this.setupSpecifiedGenderLogic()
   }
 
   chooseGender() {
-    this.extrasTarget.style.display = (this.genderTarget.value == this.letMeBeMoreSpecific()) ? "block" :  "none"
+    if (this.genderTarget.value == this.letMeBeMoreSpecific()) {
+      this.extrasTarget.style.display = "block"
+      this.specifiedTarget.focus()
+    } else {
+      this.extrasTarget.style.display = "none"
+    }
   }
 
   setupSpecifiedGenderLogic() {
