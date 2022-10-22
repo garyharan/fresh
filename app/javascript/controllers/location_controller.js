ea6ee3d6cec3432a681c2627f490013fb7d349c1
Gary Haran <gary.haran@gmail.com>
2022-10-22 10:35:44 -0400

    avoiding double clicking the find location button

--- app/javascript/controllers/location_controller.js
+++ app/javascript/controllers/location_controller.js
@@ -13,7 +13,7 @@ export default class extends Controller {
   ]
 
   connect() {
-    console.log("location controller started")
+    this.displayFilledValues()
   }
 
   askLocation(event) {
@@ -21,12 +21,16 @@ export default class extends Controller {
 
     if ('geolocation' in navigator) {
       this.locationFieldTarget.value = "loading..."
+      this.hideButton()
 
       navigator.geolocation.getCurrentPosition((success) => {
         this.latTarget.value = success.coords.latitude
         this.lonTarget.value = success.coords.longitude
 
-        fetch(this.element.getAttribute("data-location-url-value").concat("?", 'lat=', this.latTarget.value, '&', 'lon=', this.lonTarget.value))
+        var params = "".concat("?", 'lat=', this.latTarget.value, '&', 'lon=', this.lonTarget.value)
+        var url = this.element.getAttribute("data-location-url-value").concat(params)
+
+        fetch(url)
           .then(response => response.json())
           .then(json => this.populateFromGeo(json))
       }, (error) => this.explainGeoRequirement())
@@ -34,19 +38,27 @@ export default class extends Controller {
   }
 
   populateFromGeo(json) {
-    var city = json["city"]
-    var state = json["state"]
-    var country = json["country"]
+    this.cityTarget.value = json["city"]
+    this.stateTarget.value = json["state"]
+    this.countryTarget.value = json["country"]
 
-    this.locationFieldTarget.value = city + ", " + state + ", " + country
+    this.displayFilledValues()
+  }
 
-    this.cityTarget.value = city
-    this.stateTarget.value = state
-    this.countryTarget.value = country
-
+  displayFilledValues() {
+    this.locationFieldTarget.value = this.cityTarget.value + ", " + this.stateTarget.value + ", " + this.countryTarget.value
   }
 
   explainGeoRequirement() {
     alert("We need location services to allow us to match you with others.")
+    this.showButton()
+  }
+
+  hideButton() {
+    this.locationButtonTarget.style.display = "none"
+  }
+
+  showButton() {
+    this.locationButtonTarget.style.display = "inline"
   }
 }
