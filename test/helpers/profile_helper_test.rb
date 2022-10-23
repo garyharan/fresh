3853f1903deb8d45aa5a0484ae7a91dd12909eea
Gary Haran <gary.haran@gmail.com>
2022-10-22 21:23:59 -0400

    fixing assertion typo

--- test/helpers/profile_helper_test.rb
+++ test/helpers/profile_helper_test.rb
@@ -2,6 +2,6 @@ require "test_helper"
 
 class ProfilesHelperTest < ActionView::TestCase
   test "#imperialize should convert to imperial" do
-    assert_equal "5'10", imperialize(178)
+    assert_equal "5'10\"", imperialize(178)
   end
 end
