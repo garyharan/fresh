d15133ce200c964a402aded47d4789056175e269
Gary Haran <gary.haran@gmail.com>
2022-11-29 20:38:45 -0500

    added tests for likes controller

--- test/controllers/likes_controller_test.rb
+++ test/controllers/likes_controller_test.rb
@@ -1,13 +1,22 @@
 require "test_helper"
 
 class LikesControllerTest < ActionDispatch::IntegrationTest
+  setup { sign_in users(:gathino) }
+
   test "should get create" do
-    get likes_create_url
-    assert_response :success
+    assert_difference "Like.count" do
+      post profile_likes_url(profiles(:two), format: :turbo_stream)
+    end
   end
 
   test "should get destroy" do
-    get likes_destroy_url
-    assert_response :success
+    like =
+      Like.create(
+        profile_id: profiles(:two).id,
+        author_profile_id: profiles(:one).id
+      )
+    assert_difference "Like.count", -1 do
+      delete profile_like_url(profiles(:two), like, format: :turbo_stream)
+    end
   end
 end
