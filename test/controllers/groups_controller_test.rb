769b80bdd28f5172314984f6c808ba9c7e5e35f9
Gary Haran <gary.haran@gmail.com>
2022-12-13 07:43:02 -0500

    adding tests for groups

--- test/controllers/groups_controller_test.rb
+++ test/controllers/groups_controller_test.rb
@@ -1,8 +1,17 @@
 require "test_helper"
 
 class GroupsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    sign_in users(:gathino)
+  end
+
   test "should get index" do
     get groups_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get group_url(groups(:one))
     assert_response :success
   end
 end
