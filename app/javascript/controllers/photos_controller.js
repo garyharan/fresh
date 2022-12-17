e1a23e790ef8cb3810c32b6db652185c5262ea42
Gary Haran <gary.haran@gmail.com>
2022-12-16 23:03:54 -0500

    cleaning up

--- app/javascript/controllers/photos_controller.js
+++ app/javascript/controllers/photos_controller.js
@@ -4,9 +4,6 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = [ "submitButton", "uploadSection", "spinner" ]
 
-  connect() {
-  }
-
   submit(event) {
     this.submitButtonTarget.click()
     this.uploadSectionTarget.classList.add("hidden")
