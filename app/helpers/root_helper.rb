bbc68df5458818c5481edc15321073db29e6c2d4
Gary Haran <gary.haran@gmail.com>
2022-12-16 19:55:10 -0500

    using more generic error message for profile

--- app/helpers/root_helper.rb
+++ app/helpers/root_helper.rb
@@ -1,2 +1,9 @@
 module RootHelper
+  def error_messages_for(record, attribute)
+    return "" unless record.errors[attribute].any?
+
+    content_tag :div, class: "text-red-500 text-sm" do
+      record.errors[attribute].join(", ")
+    end
+  end
 end
