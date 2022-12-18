f7a0c4bfa57ecf4334daf1bb357e4acb5255afa2
Gary Haran <gary.haran@gmail.com>
2022-12-17 22:42:50 -0500

    cleaning up and allowing nil age to break profile index page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,21 +1,7 @@
 module ApplicationHelper
-  def display_name(user)
-    if user.profile && user.profile.display_name
-      user.profile.display_name
-    else
-      user.email
-    end
-  end
-
-  def avatar_image_url(user)
-    user.profile.images.first if user.profile && user.profile.images.any?
-  end
-
-  def gender_choices
-    Profile::POSSIBLE_GENDERS
-  end
-
   def age(dob)
+    return "??" if dob.nil?
+
     now = Time.now.utc.to_date
     now.year - dob.year -
       (
--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -1,23 +1,9 @@
 require "test_helper"
 
 class ApplicationHelperTest < ActionView::TestCase
-  test "should show display name if profile present" do
-    user = users(:gathino)
-    assert_equal "Gathino", display_name(user)
-  end
-
-  test "should default to email if display_name is not created yet" do
-    user = users(:velvet)
-    assert_equal "velvet@gmail.com", display_name(user)
-  end
-
-  test "#gender_choice should show default choices" do
-    assert_equal ["Woman", "Man", "Non-Binary and/or Two Spirit Person"],
-                 gender_choices
-  end
-
   test "#age should show appropriate age" do
     assert_equal 17, age(17.years.ago)
     assert_equal 18, age(18.years.ago)
+    assert_equal "??", age(nil)
   end
 end
