9f43d8600570d324ff252bdf7d2e6e2f73c97dbe
Gary Haran <gary.haran@gmail.com>
2022-12-20 12:23:54 -0500

    giving myself usable seeds for development and testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-puts "Adding genders"
+puts "Adding genders" if Rails.env.production? || Rails.env.staging?
 
 if Gender.count.zero?
   Gender.create(
@@ -20,28 +20,31 @@ else
   puts "Already filled genders"
 end
 
-if Rails.env.development?
+if Rails.env.development? || Rails.env.test?
   if User.count < 10
     (0..10).each do |number|
       u =
-        User.create! email: Faker::Internet.email,
-                     password: "asd0fadsfh",
-                     confirmation_token: Devise.friendly_token
+        User.new email: Faker::Internet.email,
+                    password: "asd0fadsfh",
+                    confirmation_token: Devise.friendly_token
+      u.skip_confirmation!
+      u.save(validate: false)
 
       p =
-        Profile.create! user_id: u.id,
-                        display_name: Faker::Name.name,
-                        born_on: (18..99).to_a.sample.years.ago,
-                        gender_id: [1, 2, 3].sample,
-                        city: ['Longueuil', 'Montreal', 'Laval'].sample,
-                        state: 'Quebec',
-                        country: 'Canada',
-                        gender_ids: Gender.all.sample(2).map(&:id),
-                        children: Profile::POSSIBLE_CHILDREN_CONFIGURATIONS.sample,
-                        relationship_style: Profile::POSSIBLE_RELATIONSHIP_STYLES.sample,
-                        drinking: Profile::POSSIBLE_DRINKING_OPTIONS.sample,
-                        smoking: Profile::POSSIBLE_SMOKING_OPTIONS.sample,
-                        height: (150..200).to_a.sample
+        Profile.new user_id: u.id,
+                       display_name: Faker::Name.name,
+                       born_on: (18..99).to_a.sample.years.ago,
+                       gender_id: Gender.all.to_a[number % 3].id,
+                       gender_ids: Gender.all.sample(2).map(&:id),
+                       city: ['Longueuil', 'Montreal', 'Laval'].sample,
+                       state: 'Quebec',
+                       country: 'Canada',
+                       children: Profile::POSSIBLE_CHILDREN_CONFIGURATIONS.sample,
+                       relationship_style: Profile::POSSIBLE_RELATIONSHIP_STYLES.sample,
+                       drinking: Profile::POSSIBLE_DRINKING_OPTIONS.sample,
+                       smoking: Profile::POSSIBLE_SMOKING_OPTIONS.sample,
+                       height: (150..200).to_a.sample
+      p.save(validate: false)
 
 
       image = p.images.create
