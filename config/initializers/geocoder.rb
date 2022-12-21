2df1b943325b8b5082908f3e2e750138950dfbcb
Gary Haran <gary.haran@gmail.com>
2022-12-21 07:53:41 -0500

    caching geocoder to avoid having more calls for a given city

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -5,9 +5,10 @@ Geocoder.configure(
   # api_key: Rails.application.credentials.google_maps.api_key,
   language: :en,
   units: :km
+
   # Cache configuration
-  # cache_options: {
-  #   expiration: 2.days,
-  #   prefix: 'geocoder:'
-  # }
+  cache_options: {
+    expiration: nil,
+    prefix: 'geocoder:'
+  }
 )
