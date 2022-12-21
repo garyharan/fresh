90312cd5387a30235aae7e972c617e3a36303cda
Gary Haran <gary.haran@gmail.com>
2022-12-21 15:24:48 -0500

    allowing liked profiles and passed profiles to be listed from user

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,15 @@ class User < ApplicationRecord
   has_many :managed_groups, class_name: "Group", foreign_key: :user_id
   has_many :memberships, dependent: :destroy
   has_many :groups, through: :memberships
+
+  has_many :likes, foreign_key: :user_id, dependent: :destroy
+  has_many :passes, foreign_key: :user_id, dependent: :destroy
+
+  def liked_profiles
+    Profile.joins(:likes).where(likes: { user_id: id })
+  end
+
+  def passed_profiles
+    Profile.joins(:passes).where(passes: { user_id: id })
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,21 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "#liked_profiles finds all liked profile" do
+    gathino = users(:gathino)
+    velvet = users(:velvet)
+
+    gathino.likes.create! profile: velvet.profile
+
+    assert_equal [profiles(:two)], gathino.liked_profiles
+  end
+
+  test "#passed_profiles finds all passed profile" do
+    gathino = users(:gathino)
+    velvet = users(:velvet)
+
+    gathino.passes.create! profile: velvet.profile
+
+    assert_equal [profiles(:two)], gathino.passed_profiles
+  end
 end
