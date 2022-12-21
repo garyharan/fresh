c7ce62a467cf4231265b98f76c47811ee2d45ea0
Gary Haran <gary.haran@gmail.com>
2022-12-21 08:40:47 -0500

    Like is not responsible for creating rooms anymore

--- test/models/like_test.rb
+++ test/models/like_test.rb
@@ -1,18 +1,4 @@
 require "test_helper"
 
 class LikeTest < ActiveSupport::TestCase
-  test "a room gets created when both profiles like one another" do
-    gathino = profiles(:one)
-    velvet = profiles(:two)
-
-    Room.destroy_all
-    Like.destroy_all
-
-    assert_difference "Room.count", 1 do
-      Like.create! profile: velvet, author_profile: gathino
-      Like.create! profile: gathino, author_profile: velvet
-    end
-
-    assert_equal 2, Like.count
-  end
 end
