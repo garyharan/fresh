eb9e036e4447e13413634d1057159b0e960355d4
Gary Haran <gary.haran@gmail.com>
2022-12-23 12:35:01 -0500

    refactoring forcing completion

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,17 @@
 class ApplicationController < ActionController::Base
   include ActiveStorage::SetCurrent
 
-
   def after_sign_in_path_for(resource)
     if resource.is_a?(User) && resource.profile.nil?
       onboarding_one_url
     else
       super
     end
+  end
+
+  private
+
+  def force_profile_completion
+    redirect_to new_profile_url unless current_user.profile&.complete?
   end
 end
