f8488d0b3c795e2681dd83c273ea3b7dfef046bb
Gary Haran <gary.haran@gmail.com>
2022-12-23 21:24:32 -0500

    less buggy implementation of Room.find_or_create_by_profiles

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,5 +1,6 @@
 class Room < ApplicationRecord
   has_and_belongs_to_many :profiles
+  accepts_nested_attributes_for :profiles
 
   has_many :messages, class_name: "Message", dependent: :destroy
 
@@ -8,9 +9,11 @@ class Room < ApplicationRecord
   end
 
   def self.find_or_create_by_profiles(profiles)
-    # find the room where the profile attached belongs to both profiles
-    room = Room.includes(:profiles).where(profiles: { id: profiles.map(&:id) }).first
-    room || Room.create_with_profiles(profiles)
+    Room.find_by_profiles(profiles) || Room.create_with_profiles(profiles)
+  end
+
+  def self.find_by_profiles(profiles)
+    Room.joins(:profiles).where(profiles: { id: profiles.map(&:id) }).group("rooms.id").having("count(profiles.id) = ?", profiles.size).first
   end
 
   def self.create_with_profiles(profiles)
--- test/models/room_test.rb
+++ test/models/room_test.rb
@@ -12,4 +12,28 @@ class RoomTest < ActiveSupport::TestCase
 
     assert_equal profiles.sort, room.profiles.sort
   end
+
+  test ".room_for(profiles) returns the correct profile" do
+    room1 = Room.find_or_create_by_profiles([profiles(:one), profiles(:two)])
+    room2 = Room.find_or_create_by_profiles([profiles(:two), profiles(:three)])
+    room3 = Room.find_or_create_by_profiles([profiles(:one), profiles(:three)])
+
+    assert_equal 2, room1.profiles.count
+    assert_equal 2, room2.profiles.count
+    assert_equal 2, room3.profiles.count
+
+    assert_includes room1.profiles.pluck(:display_name), profiles(:one).display_name
+    assert_includes room1.profiles.pluck(:display_name), profiles(:two).display_name
+
+    assert_includes room2.profiles.pluck(:display_name), profiles(:two).display_name
+    assert_includes room2.profiles.pluck(:display_name), profiles(:three).display_name
+
+    assert_includes room3.profiles.pluck(:display_name), profiles(:one).display_name
+    assert_includes room3.profiles.pluck(:display_name), profiles(:three).display_name
+
+
+    assert_equal [profiles(:one), profiles(:two)].sort,   room1.profiles.sort
+    assert_equal [profiles(:two), profiles(:three)].sort, room2.profiles.sort
+    assert_equal [profiles(:one), profiles(:three)].sort, room3.profiles.sort
+  end
 end
