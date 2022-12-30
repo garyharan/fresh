f0698c5a6009a5e7a7952f186b80652d9ec70346
Gary Haran <gary.haran@gmail.com>
2022-12-30 18:09:21 -0500

    trying this again

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,5 +8,5 @@ Rails.application.config.assets.version = "1.0"
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
-# folder are already added.
-# Rails.application.config.assets.precompile += %w( admin.js admin.css )
+# folder are already added.  ### NOT TRUE FOR ME
+ Rails.application.config.assets.precompile += %w( application.js application.css )
