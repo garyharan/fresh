f208981d052bf81de76a4f47bbb4d35bf8b8fbce
Gary Haran <gary.haran@gmail.com>
2023-01-03 12:55:33 -0500

    easier to read tailwind config

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,11 +1,11 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
     './public/*.html',
-
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}',
+    './app/views/**/*.erb',
     './node_modules/flowbite/**/*.js',
   ],
   theme: {
