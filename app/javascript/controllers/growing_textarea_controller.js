b975b2b05357143f10315e4dd25b7c8ca3a7fd41
Gary Haran <gary.haran@gmail.com>
2023-01-05 12:26:11 -0500

    submission via button still resets form

--- app/javascript/controllers/growing_textarea_controller.js
+++ app/javascript/controllers/growing_textarea_controller.js
@@ -6,6 +6,8 @@ export default class extends Controller {
     this.element.addEventListener("keyup", this.handleResize.bind(this))
     this.element.addEventListener("keydown", this.handleSubmission.bind(this))
 
+    this.element.form.addEventListener("turbo:submit-end", this.resetField.bind(this))
+
     this.originalHeight = this.element.style.height
   }
 
@@ -14,11 +16,12 @@ export default class extends Controller {
   }
 
   handleSubmission(event) {
-    if (event.keyCode === 13 && event.metaKey || event.keyCode === 13 && event.ctrlKey) {
-      event.preventDefault()
-
-      this.submitForm()
-      this.resetField()
+    if (!this.isMobileClient()) {
+      if (event.keyCode === 13 && !event.shiftKey) {
+        event.preventDefault()
+        this.submitForm()
+        this.resetField()
+      }
     }
   }
 
@@ -29,5 +32,13 @@ export default class extends Controller {
   resetField() {
     this.element.value = ""
     this.element.style.height = this.originalHeight
+  }
+
+  isMobileClient() {
+    if (navigator.userAgendData && navigator.userAgentData.mobile) {
+      return true
+    }
+
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
   }
 }
