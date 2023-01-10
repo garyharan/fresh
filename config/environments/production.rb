dd3a497b96127b58b33ee3085ffe7d933a74932f
Gary Haran <gary.haran@gmail.com>
2023-01-10 11:41:09 -0500

    configuring email with sendgrid for production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,16 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { host: "fresh-production.onrender.com" }
+  config.action_mailer.default_url_options = { host: "fresh.dating" }
+  config.action_mailer.smtp_settings = {
+    :user_name => 'apikey',
+    :password => ENV['SENDGRID_API_KEY'],
+    :domain => 'fresh.dating',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
