10e4b742258841f1fa5274769b3b693747daf6c6
Gary Haran <gary.haran@gmail.com>
2023-01-10 14:01:33 -0500

    trying to compile assets on the fly

--- render-build.sh
+++ render-build.sh
@@ -5,7 +5,9 @@ bundle install
 
 bundle exec rails tailwindcss:clobber
 bundle exec rails tailwindcss:build
+
 yarn build
 yarn build:css
+
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
