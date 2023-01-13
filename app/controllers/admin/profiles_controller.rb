6712c699e5340dadf5c5b03de5d21215611300fd
Gary Haran <garyharan@users.noreply.github.com>
2023-01-13 15:16:20 -0500

    added admin section to see all existing profiles

--- app/controllers/admin/profiles_controller.rb
+++ app/controllers/admin/profiles_controller.rb
@@ -1,5 +1,14 @@
 class Admin::ProfilesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :authenticate_admin!
+
   def index
     @profiles = Profile.page(params[:page]).per(10)
+  end
+
+  private
+
+  def authenticate_admin!
+    redirect_to root_path, alert: 'Not authorized.' unless current_user.admin?
   end
 end
--- test/controllers/admin/profiles_controller_test.rb
+++ test/controllers/admin/profiles_controller_test.rb
@@ -1,8 +1,26 @@
 require "test_helper"
 
 class Admin::ProfilesControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get admin_profiles_index_url
+  test "should get redirected to authentication if not logged in" do
+    get admin_profiles_url
+    assert_response :redirect
+  end
+
+  test "normal users gets told they cannot see this page" do
+    sign_in users(:gathino)
+    get admin_profiles_url
+
+    assert_response :redirect
+    assert_equal "Not authorized.",  flash[:alert]
+  end
+
+  test "only admins can see this page" do
+    @user = users(:gathino)
+    @user.update_column :admin, true
+
+    sign_in @user
+    get admin_profiles_url
+
     assert_response :success
   end
 end
