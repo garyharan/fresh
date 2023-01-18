6a10b086946aa8e099c225e62fa40fe15b9f3aca
Gary Haran <garyharan@users.noreply.github.com>
2023-01-18 11:31:40 -0500

    fixed another scrolling messages bug

--- app/javascript/controllers/message_controller.js
+++ app/javascript/controllers/message_controller.js
@@ -15,7 +15,7 @@ export default class extends Controller {
 
     this.setBubbleBottom()
 
-    if (!this.element.getAttribute("data-respress-scroll") === "true") {
+    if (!this.element.getAttribute("data-repress-scroll") === "true") {
       this.scrollToMessage()
     }
   }
