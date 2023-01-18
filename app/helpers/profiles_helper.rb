28c3e91d68b9f00c03085f5ea4f66c2d0c331d05
Gary Haran <garyharan@users.noreply.github.com>
2023-01-18 11:59:21 -0500

    Freshness details

--- app/helpers/profiles_helper.rb
+++ app/helpers/profiles_helper.rb
@@ -4,4 +4,18 @@ module ProfilesHelper
     feet, inches = inches.divmod(12)
     "#{feet}'#{inches.floor}\""
   end
+
+  def online_in_the_last_period(profile)
+    last_sign_in = profile.user.last_sign_in_at
+
+    if 24.hours.ago < last_sign_in
+      "Online in the last day"
+    elsif 7.days.ago < last_sign_in
+      "Online in the last week"
+    elsif 30.days.ago < last_sign_in
+      "Online in the last month"
+    else
+      "Online over a month ago"
+    end
+  end
 end
--- test/helpers/profile_helper_test.rb
+++ test/helpers/profile_helper_test.rb
@@ -4,4 +4,20 @@ class ProfilesHelperTest < ActionView::TestCase
   test "#imperialize should convert to imperial" do
     assert_equal "5'10\"", imperialize(178)
   end
+
+  test "#online_in_the_last_period should return a human readable string" do
+    user = users(:gathino)
+    user.update_attribute(:last_sign_in_at, 23.hours.ago)
+
+    assert_equal "Online in the last day", online_in_the_last_period(user.profile)
+
+    user.update_attribute(:last_sign_in_at, 6.days.ago)
+    assert_equal "Online in the last week", online_in_the_last_period(user.profile)
+
+    user.update_attribute(:last_sign_in_at, 29.days.ago)
+    assert_equal "Online in the last month", online_in_the_last_period(user.profile)
+
+    user.update_attribute(:last_sign_in_at, 2.years.ago)
+    assert_equal "Online over a month ago", online_in_the_last_period(user.profile)
+  end
 end
