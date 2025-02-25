b7ad406eab4a2e97d99fa5de33be28ba87ee26c9
Gary Haran <garyharan@users.noreply.github.com>
2025-02-25 16:12:51 -0500

    feat: allow sign in page to offer sign up

--- app/controllers/concerns/authentication.rb
+++ app/controllers/concerns/authentication.rb
@@ -33,7 +33,12 @@ module Authentication
 
     def request_authentication
       session[:return_to_after_authenticating] = request.url
-      redirect_to new_session_path
+
+      if hotwire_native?
+        head :unauthorized
+      else
+        redirect_to new_session_path
+      end
     end
 
     def after_authentication_url
--- app/javascript/controllers/bridge/button_controller.js
+++ app/javascript/controllers/bridge/button_controller.js
@@ -3,18 +3,12 @@ import { BridgeComponent } from "@hotwired/hotwire-native-bridge"
 export default class extends BridgeComponent {
   static component = "button"
 
-  static values = {
-    title: String,
-    position: String,
-    vibrate: String
-  }
-
   connect() {
     super.connect()
 
-    const title = this.titleValue
-    const position = this.positionValue
-    const vibrate = this.vibrateValue
+    const title = this.bridgeElement.bridgeAttribute("title")
+    const position = this.bridgeElement.bridgeAttribute("position")
+    const vibrate = this.bridgeElement.bridgeAttribute("vibrate")
 
     this.send("connect", {title, position, vibrate}, () => {
       this.bridgeElement.click()
