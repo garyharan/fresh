329ed4f9a53da7d7b813283560554a4a9e270cac
Gary Haran <garyharan@users.noreply.github.com>
2025-03-19 13:14:04 -0400

    fix: liking someone who does not like you now shows next profile

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,8 +1,9 @@
 class LikesController < ApplicationController
   def create
     @profile = Profile.find(params[:profile_id])
-    @profiles = Profile.recommended(Current.user.profile)
     @like = Like.create! profile_id: params[:profile_id], user_id: Current.user.id
+
+    @profiles = Profile.recommended(Current.user.profile)
 
     if @like.reciprocated?
       @room = Room.find_or_create_by_like(@like)
@@ -13,7 +14,9 @@ class LikesController < ApplicationController
     else
       respond_to do |format|
         format.html { redirect_to profiles_path }
-        format.turbo_stream { render turbo_stream: turbo_stream.replace('discovery', partial: 'profiles/index') }
+        format.turbo_stream {
+          render turbo_stream: turbo_stream.replace('discovery', partial: 'profiles/index')
+        }
       end
     end
   end
