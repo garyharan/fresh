1ed16b129f05d3f609505fba9f9c733e4b0f2fc8
Gary Haran <garyharan@users.noreply.github.com>
2025-04-29 11:20:17 -0400

    fix: fixing tests to match new sign in helpers

--- test/controllers/onboarding_controller_test.rb
+++ test/controllers/onboarding_controller_test.rb
@@ -3,12 +3,9 @@ require "test_helper"
 class OnboardingControllerTest < ActionDispatch::IntegrationTest
   setup do
     @user = users(:gathino)
-    sign_in @user
   end
 
   test "should require authentication" do
-    sign_out @user
-
     [onboarding_one_url, onboarding_two_url, onboarding_three_url, onboarding_four_url].each do |url|
       get url
       assert_response :redirect
@@ -16,43 +13,53 @@ class OnboardingControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get one" do
+    sign_in @user
     get onboarding_one_url
     assert_response :success
   end
 
   test "should update step one" do
+    sign_in @user
+
     patch onboarding_update_one_url, params: { profile: { display_name: "Cão" } }
     assert_redirected_to onboarding_two_url
     assert @user.profile.display_name == "Cão"
   end
 
   test "should fail to update step one gracefully" do
+    sign_in @user
+
     patch onboarding_update_one_url, params: { profile: { display_name: "" } }
     assert_response :unprocessable_entity
   end
 
   test "should get two" do
+    sign_in @user
     get onboarding_two_url
     assert_response :success
   end
 
   test "should update step two" do
+    sign_in @user
     patch onboarding_update_two_url, params: { profile: { children: "Not sure yet" } }
     assert_redirected_to onboarding_three_url
     assert @user.profile.children == "Not sure yet"
   end
 
   test "should fail to update step two gracefully" do
+    sign_in @user
     patch onboarding_update_two_url, params: { profile: { children: "" } }
     assert_response :unprocessable_entity
   end
 
   test "should get three" do
+    sign_in @user
     get onboarding_three_url
     assert_response :success
   end
 
   test "should get four" do
+    sign_in @user
     get onboarding_four_url
     assert_response :success
   end
