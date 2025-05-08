acd8c5b91b10b243ae2cd4988d7dc8dfb011d651
Gary Haran <garyharan@users.noreply.github.com>
2025-05-08 15:08:09 -0400

    feat: unmatch feature plumbing

--- app/models/pass.rb
+++ app/models/pass.rb
@@ -1,4 +1,8 @@
 class Pass < ApplicationRecord
   belongs_to :profile
   belongs_to :user
+
+  before_create do
+    Like.where(user_id: user.id, profile_id: profile.id).destroy_all
+  end
 end
--- test/models/pass_test.rb
+++ test/models/pass_test.rb
@@ -1,7 +1,19 @@
 require "test_helper"
 
 class PassTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "creating a pass destroys any likes for the same profile" do
+    gathino = users(:gathino)
+    velvet  = users(:velvet)
+    eva     = users(:eva)
+
+    gathino_like = Like.create! profile: gathino.profile, user: velvet
+    velvet_like  = Like.create! profile: velvet.profile, user: eva
+    eva_like     = Like.create! profile: eva.profile, user: gathino
+
+    assert_difference "Pass.count", 1 do
+      assert_difference "Like.count", -1 do
+        Pass.create! profile: gathino.profile, user: velvet
+      end
+    end
+  end
 end
