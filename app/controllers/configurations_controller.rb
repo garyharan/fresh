48cf1a3a6323c214483ca553e571e4038b608cf4
Gary Haran <garyharan@users.noreply.github.com>
2025-05-28 21:11:42 -0400

    fix: sign in not popping modal on iOS

--- app/controllers/configurations_controller.rb
+++ app/controllers/configurations_controller.rb
@@ -76,18 +76,6 @@ class ConfigurationsController < ApplicationController
       settings: {},
       rules: [
         {
-          patterns: ["/recede_historical_location"],
-          properties: { presentation: "pop" }
-        },
-        {
-          patterns: ["/resume_historical_location"],
-          properties: { presentation: "none" }
-        },
-        {
-          patterns: ["/refresh_historical_location"],
-          properties: { presentation: "refresh" }
-        },
-        {
           patterns: [
             "/profiles$",
             "/profile$",
