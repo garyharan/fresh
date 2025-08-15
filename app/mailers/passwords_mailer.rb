f09b64116f47154c3061d3ffa6c8f49c59466068
Gary Haran <garyharan@users.noreply.github.com>
2025-08-15 12:16:12 -0400

    fix: Configuration for mailer address to fit our domain

--- app/mailers/passwords_mailer.rb
+++ app/mailers/passwords_mailer.rb
@@ -1,4 +1,6 @@
 class PasswordsMailer < ApplicationMailer
+  default from: "admin@fresh.dating"
+
   def reset(user)
     @user = user
     mail subject: "Reset your password", to: user.email_address
