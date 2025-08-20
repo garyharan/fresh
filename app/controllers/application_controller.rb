a4c20c03cdf083ab6f87bb0c4e2e230c23ab5d63
Gary Haran <garyharan@users.noreply.github.com>
2025-08-20 11:04:23 -0400

    Feat: French language translation for the home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < ActionController::Base
 
   helper_method :hotwire_native?
 
+  before_action :set_locale
+
   def hotwire_native?
     !!(request.user_agent =~ /Hotwire Native/)
   end
@@ -13,5 +15,24 @@ class ApplicationController < ActionController::Base
 
   def force_profile_completion
     redirect_to new_profile_url unless Current.user&.profile&.complete?
+  end
+
+  SUPPORTED_LOCALES = %i[en fr]
+
+  def set_locale
+    I18n.locale = requested_locale || I18n.default_locale
+  end
+
+  def requested_locale
+    locale = params[:locale] || locale_from_accept_language_header
+    locale if locale.present? && SUPPORTED_LOCALES.include?(locale.to_sym)
+  end
+
+  def locale_from_accept_language_header
+    request.env['HTTP_ACCEPT_LANGUAGE']&.scan(/^[a-z]{2}/)&.first&.to_sym
+  end
+
+  def default_url_options
+    { locale: I18n.locale }
   end
 end
--- test/controllers/application_controller_test.rb
+++ test/controllers/application_controller_test.rb
@@ -5,4 +5,24 @@ class ApplicationControllerTest < ActionDispatch::IntegrationTest
     get root_url, headers: { "User-Agent" => "Hotwire Native" }
     assert @controller.view_context.hotwire_native?
   end
+
+  test "should set locale from params" do
+    get root_url(locale: "fr")
+    assert_equal :fr, I18n.locale
+  end
+
+  test "should set locale from Accept-Language header" do
+    get root_url, headers: { "Accept-Language" => "fr,en;q=0.9" }
+    assert_equal :fr, I18n.locale
+  end
+
+  test "should fallback to English requested locale by param if unsupported" do
+    get root_url(locale: "cn")
+    assert_equal I18n.default_locale, I18n.locale
+  end
+
+  test "should fallback to English if Accept-Language header is unsupported" do
+    get root_url, headers: { "Accept-Language" => "es" }
+    assert_equal I18n.default_locale, I18n.locale
+  end
 end
