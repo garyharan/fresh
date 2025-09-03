50249b9de28b6d2c187a7298d3259f970af4e64d
Gary Haran <garyharan@users.noreply.github.com>
2025-09-03 11:43:17 -0400

    chore: Fuck everything about foreign keys in fixtures
    This allows us to ignore issues with fixtures loading order.

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative "boot"
+require_relative 'boot'
 
-require "rails/all"
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -23,5 +23,7 @@ module Fresh
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.active_record.verify_foreign_keys_for_fixtures = false
   end
 end
