f0508e30f74103de5f7b103d5c9be15c3336f43f
Gary Haran <garyharan@users.noreply.github.com>
2025-09-09 17:12:10 -0400

    feat: Geolocating event locations

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,5 +4,8 @@ class Event < ApplicationRecord
   has_many :attendances
   has_many :users, through: :attendances
 
+  geocoded_by :location
+  after_validation :geocode, if: ->(obj) { obj.location_changed? }
+
   validates_presence_of :name, :location, :start_time, :end_time
 end
--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -1,7 +1,19 @@
 require "test_helper"
 
 class EventTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "calculates the latitude and longtitude after validation if the location changed" do
+    event = Event.new(
+      name: "Sample Event",
+      location: "New York, NY",
+      start_time: Time.now + 1.day,
+      end_time: Time.now + 1.day + 2.hours,
+      creator: users(:gathino)
+    )
+    assert_nil event.latitude
+    assert_nil event.longitude
+
+    event.valid? # Trigger validations and geocoding
+    assert_not_nil event.latitude
+    assert_not_nil event.longitude
+  end
 end
